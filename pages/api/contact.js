2cc9aa7c9f86fc44b5e42d8ae1359f7e09bf735f
Kavin Valli <kavinvalli@gmail.com>
2021-09-13 07:26:48 +0530

    feat: Move contact api to mailgun

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,31 +1,43 @@
 export default function handler(req, res) {
-  let nodemailer = require("nodemailer");
-  require("dotenv").config();
-  const transporter = nodemailer.createTransport({
-    port: 465,
-    host: "smtp.gmail.com",
-    auth: {
-      user: process.env.GMAIL_EMAIL,
-      pass: process.env.GMAIL_PASSWORD,
-    },
-    secure: true,
-  });
+  // let nodemailer = require("nodemailer");
+  // require("dotenv").config();
+  // const transporter = nodemailer.createTransport({
+  //   port: 465,
+  //   host: "smtp.gmail.com",
+  //   auth: {
+  //     user: process.env.GMAIL_EMAIL,
+  //     pass: process.env.GMAIL_PASSWORD,
+  //   },
+  //   secure: true,
+  // });
 
-  const mailData = {
-    from: process.env.GMAIL_EMAIL,
-    to: ["mail@kavin.me", "kavinvalli@gmail.com"],
+  // const mailData = {
+  //   from: process.env.GMAIL_EMAIL,
+  //   to: ["mail@kavin.me", "kavinvalli@gmail.com"],
+  //   subject: `Message from ${req.body.name}`,
+  //   text: req.body.message,
+  //   html: `<div>${req.body.message}</div>`,
+  // };
+
+  // transporter.sendMail(mailData, (err, info) => {
+  //   if (err) {
+  //     console.log(err);
+  //     return res.status(200).json({ error: err.toString() });
+  //   } else {
+  //     console.log(info);
+  //   }
+  // });
+  const mailgun = require("mailgun-js");
+  const DOMAIN = 'mg.kavin.me';
+  const mg = mailgun({apiKey: process.env.MAILGUN_API_KEY, domain: DOMAIN});
+  const data = {
+    from: 'Kavin Website <website@mg.kavin.me>',
+    to: 'mail@kavin.me, kavinvalli@gmail.com',
     subject: `Message from ${req.body.name}`,
     text: req.body.message,
-    html: `<div>${req.body.message}</div>`,
   };
-
-  transporter.sendMail(mailData, (err, info) => {
-    if (err) {
-      console.log(err);
-      return res.status(200).json({ error: err.toString() });
-    } else {
-      console.log(info);
-    }
+  mg.messages().send(data, function (_, body) {
+    console.log(body);
   });
 
   return res.status(200).json({});
